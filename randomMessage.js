b553eeb2216a1e0bb6c8915ff6cfbc8e0ac4e83b
Yoon Jae <ys738@cornell.edu>
2021-06-23 16:55:21 +0900

    added randomNum function

--- randomMessage.js
+++ randomMessage.js
@@ -1,3 +1,14 @@
-let randomMessage = "i love you";
-console.log(randomMessage);
-console.log('jk');
+// Let's make a program that generates a sentence that is composed of 
+//1) personal pronoun 
+//2) adverb 
+//3) noun 
+
+let lsPronoun = []; 
+let lsAdverb = [];
+let lsNoun = [];
+
+function randomNum(list){
+    let l = list.length; 
+    const num = Math.floor(((Math.random()+1)*l))
+    return num;
+}
